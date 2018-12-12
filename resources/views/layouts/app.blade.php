e50a4f813be3ea80197d5efb6960fb5e12adf19b
iamjunior <myjuniorprog@gmail.com>
2018-12-12 17:23:54 +0700

    video 11

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,7 +57,10 @@
                         </li>
                         <li class="nav-item">
                                <a class="nav-link" href="{{ route('tiket.index')}}">Data Tiket</a>
-                       </li>
+                        </li>
+                        <li class="nav-item">
+                               <a class="nav-link" href="{{ route('transaksi.index')}}">Transaksi</a>
+                        </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
