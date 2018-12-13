9842a3fa58da74bb1de229b275d8f8016a21c7ac
iamjunior <myjuniorprog@gmail.com>
2018-12-13 15:10:05 +0700

    video 17

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Auth::routes(['verify' => true]);
+
+Route::group(['middleware'  => 'auth'], function() {
+
 Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 Route::resource('kategori','KategoriController');
 Route::resource('tiket','TiketController');
@@ -24,3 +27,5 @@ Route::post('transaksi','TransaksiController@store')->name('transaksi.store');
 Route::delete('transaksi/{id}','TransaksiController@destroy')->name('transaksi.destroy');
 Route::get('transaksi/update','TransaksiController@update')->name('transaksi.update');
 Route::get('transaksi/pdf','TransaksiController@laporan')->name('transaksi.laporan');
+
+});
